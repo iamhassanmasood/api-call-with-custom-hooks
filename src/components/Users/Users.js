5f5b49b79a86ec36b72a5be2dde1e52ebf06ca92
Hassan Masood <iamhassanmasood@gmail.com>
2023-06-05 11:30:59 +0500

    feat: some changes in card

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -6,23 +6,25 @@ import "./Users.less";
 import Loader from "../Loader/Loader";
 
 const { Text } = Typography;
+const { Meta } = Card;
 
 const Users = () => {
   const { loading, data } = useApi("users");
 
   const UserRecords = data?.map((user, index) => (
-    <Col span={6} key={index}>
-      <Card className="card-content" title={user.name}>
-        <Row>
-          <Text strong>Username:</Text>
-          <Text>{user.username}</Text>
-        </Row>
-        <Row>
-          <Text strong>Email:</Text>
-          <Text>{user.email}</Text>
-        </Row>
-      </Card>
-    </Col>
+    <Card
+      hoverable
+      style={{ width: 240, margin: "5px" }}
+      cover={
+        <img
+          alt="example"
+          src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+        />
+      }
+      key={index}
+    >
+      <Meta title={user.name} description={user.email} />
+    </Card>
   ));
 
   if (loading) return <Loader />;
